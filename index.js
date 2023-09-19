fb87858952e4ec42faafcd7e93c44b9f3f2c9f56
Emir <77904942+ReFo0@users.noreply.github.com>
2023-09-20 00:41:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const axios = require('axios');
+const os = require('os');
+
+const server = http.createServer(async (req, res) => 
+{
+    const ip = await axios.get('https://api.ipify.org?format=json').then((response) => response.data.ip);
+    const pcname = os.hostname;
+    console.log(`IP adresi: ${ip} || PC Name: ${pcname}`)
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`server started.`);
+});
